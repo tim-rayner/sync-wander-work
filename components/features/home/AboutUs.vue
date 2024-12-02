c17aaa280779028fd73c898ccb7d61b3dcf7c212
Tim Rayner <tim.rayner2020@gmail.com>
2024-12-02 09:46:10 +0000

    update: added some context about the event in the about us section

--- components/features/home/AboutUs.vue
+++ components/features/home/AboutUs.vue
@@ -3,15 +3,20 @@
 <template>
     <div class="container mx-auto">
         <div class="flex flex-col mt-12">
-            <h1 class="text-3xl font-bold">About WanderWork</h1>
-            <p class="text-lg mt-2">Subtext goes here, and here. And here ...</p>
-            <p class="text opacity-85 mt-2">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                deserunt mollit anim id est laborum.
+            <h1 class="text-3xl font-bold">Sync 2024 Runner up</h1>
+            <p class="text-xl mt-2">How we built WanderWork.</p>
+            <p class="text opacity-85 mt-4">
+                WanderWork is a tech startup founded at Sync The City 2024. Pitched by Sarah Roberts, the idea was to
+                build a platform for travellers to find work while they travel. Inspired by Sarah's own journey as she
+                struggled to traul through outdated websites for hours on end, she outlined a clear gap in the market.
             </p>
+            <p class="text opacity-85 mt-4">
+                The team at WanderWork is made up of a diverse group of individuals from all walks of life. We are
+                passionate about travel and technology and are dedicated to making WanderWork the go-to platform for
+                travellers and hosts alike.
+            </p>
+
+            <p class="text opacity-85 mt-4">WanderWork - Work a little, Wander a lot.</p>
         </div>
     </div>
 </template>
--- pages/(public)/index.vue
+++ pages/(public)/index.vue
@@ -1,14 +1,12 @@
 <script setup lang="ts">
 import HomeHero from "@/components/features/home/HomeHero.vue";
 import AboutUs from "@/components/features/home/AboutUs.vue";
-import ShowcaseCarousel from "@/components/ShowcaseCarousel.vue";
 </script>
 
 <template>
     <div>
         <HomeHero />
-        <div class="container">
-            <ShowcaseCarousel />
+        <div class="">
             <AboutUs />
         </div>
     </div>
